960bfc285002163b95b7628def071bf2966facf6
Avrama Blackwell <kblackw1@gmu.edu>
2023-02-10 13:29:29 -0500

    Sequence Task: more pythonic way of counting actions 	bias second level choice prob with Da_factor

--- SequenceTask.py
+++ SequenceTask.py
@@ -110,7 +110,7 @@ class RL:
         return state0num,state1num,matching_state1
             
     def count_actions(self,allresults,sa_combo,event_subset,accum_type='mean'): ####### Not part of RL_TD2Q
-        #2021 jan 4: added multiply rewared by events_per_trial to get mean reward per trial
+        #2021 jan 4: added multiply reward by events_per_trial to get mean reward per trial
         trial_subset=event_subset/self.agent.events_per_trial
         for sa in sa_combo:
             state=sa[0]
@@ -118,15 +118,18 @@ class RL:
             state0num,state1num,matching_state1=self.get_statenum(state)
             #count how many times that state=state and action=action
             #print('sa',sa,'matching states',matching_state1)
-            timeframe={'End':range(-event_subset,0),'Beg':range(event_subset)}
+            timeframe={'Beg':range(event_subset),'End':range(-event_subset,0)}
+            actions=np.array(self.results['action'])
             for tf,trials in timeframe.items():
                 sa_count=0
-                for tr in trials:
-                    if self.results['action'][tr]==anum:
-                        #count number of times that agent state is state0 and state1
-                        if (state[0]=='*' or self.results['state'][tr][0]==state0num) and \
-                            (self.results['state'][tr][1]==state1num or self.env.state_from_number(self.results['state'][tr])[1] in matching_state1):
-                            sa_count+=1
+                action_indices=np.where(actions[trials]==anum)[0]+trials[0] #indices with correct actions
+                #for tr in trials:
+                for tr in action_indices:
+                    #if self.results['action'][tr]==anum:
+                    #count number of times that agent state is state0 and state1
+                    if (state[0]=='*' or self.results['state'][tr][0]==state0num) and \
+                        (self.results['state'][tr][1]==state1num or self.env.state_from_number(self.results['state'][tr])[1] in matching_state1):
+                        sa_count+=1
                 allresults[sa][tf].append(sa_count/trial_subset) #events per trial, fraction of responses in specified number of events
         if accum_type=='count':
             max_rwd=np.max(self.results['reward'])             
@@ -205,81 +208,78 @@ def accum_Qhx(states,actions,rl,numQ,Qhx=None):
 
 ##########################################################
 if __name__ == "__main__":
-    numtrials=600
-    runs=10
+    from SequenceTaskParam import Hx_len,rwd
+    from SequenceTaskParam import params,states,act
+    from SequenceTaskParam import Tloc,R,env_params
+
+    numtrials=600 # 450 #
+    runs=15
     #If want to add reward and time since reward to cues, need to divide by ~100
-    noise=0.0 #make noise small enough or state_thresh small enough to minimize new states in acquisition
+    noise=0.01 #make noise small enough or state_thresh small enough to minimize new states in acquisition
     #control output
     printR=False #print environment Reward matrix
     Info=False #print information for debugging
     plot_hist=0#1: plot Q, 2: plot the time since last reward
+    other_plots=True
     save_data=True #write output data in npz file
     Qvalues=[1,2] #simulate using these values for numQ, make this [2] to simulate inactivation
     inactivate=None #set to None to skip the inactivation test at the end 'D1', 'D2'
-    inactivate_blocks=0#3 for inactivate = 'D1' or 'D2'
+    inactivate_blocks=0# 1 or 3 for inactivate = 'D1' or 'D2', 0 otherwise
 
     ########## Plot Q values over time for these states and actions 
-    plot_Qhx=True       
+    plot_Qhx=True    
     actions_colors={'goL':'r','goR':'b','press':'k','goMag':'grey'}
 
-    from SequenceTaskParam import Hx_len,rwd
     if Hx_len==3:
         #MINIMUM actions for reward = 6, so maximum rewards = 1 per 6 "trials"
         state_action_combos=[(('*','*LL'), 'goR'),(('Rlever','*LL'),'press'),(('Rlever','LLR'),'press')]
-        events_per_trial=6
     elif Hx_len==4:
         #MINIMUM actions for reward = 7, so maximum rewards = 1 per 7 "trials"
-        state_action_combos=[(('Llever','**-L'), 'press'),(('Llever','**RL'), 'press'),(('Llever','**LL'), 'goR'),(('Rlever','**LL'),'press'),(('Rlever','*LLR'),'press'),(('Rlever','LLRR'),'goMag')]
+        state_action_combos=[(('Llever','---L'), 'press'),(('Llever','**RL'), 'press'),(('Llever','**LL'), 'goR'),(('Rlever','**LL'),'press'),(('Rlever','*LLR'),'press'),(('Rlever','LLRR'),'goMag')]
         overstay=[(('Llever','**LL'), act) for act in ['goL','goMag','press','other']]+\
-            [(('Rlever','LLRR'),act) for act in ['goL','goR','press','other']]
+            [(('Rlever','LLRR'),act) for act in ['goL','goR','press','other']]   #'**LL' instead of --LL
         premature=[(('Llever','**RL'), act) for act in ['goL','goR','goMag','other']]+\
-            [(('Llever','**-L'), act) for act in ['goL','goR','goMag','other']]+\
-            [(('Rlever','*LLR'), act) for act in ['goL','goR','goMag','other']]
+            [(('Rlever','**RL'), act) for act in ['goR','goMag','other','press']]+\
+            [(('Llever','---L'), act) for act in ['goL','goR','goMag','other']]+\
+            [(('Rlever','---L'), act) for act in ['goR','goMag','other','press']]+\
+           [(('Rlever','*LLR'), act) for act in ['goL','goR','goMag','other']] #'*LLR' instead of -LLR
         start=[(('mag','----'), act) for act in ['goL','goR','goMag','press','other']]
         state_action_combos=state_action_combos+overstay+premature+start
         sa_errors={'stay':overstay,'switch':premature,'start':start}
-        events_per_trial=7
     else:
         print('unrecognized press history length')
-    max_rwd=rwd['base']*(events_per_trial-1)+rwd['reward']  
+    
     plot_Qstates=[state[0] for state in state_action_combos]
-    numevents=numtrials*events_per_trial #number of events/actions allowed for agent per run/trial
-    trial_subset=int(0.05*numevents)# display mean reward and count actions over 1st and last of these number of trials 
+    numevents=numtrials*params['events_per_trial'] #number of events/actions allowed for agent per run/trial
+    trial_subset=int(0.05*numtrials)*params['events_per_trial']# display mean reward and count actions over 1st and last of these number of trials 
     epochs=['Beg','End']
    
     trials_per_block=10
-    events_per_block=trials_per_block* events_per_trial
+    events_per_block=trials_per_block* params['events_per_trial']
     num_blocks=int((numevents+1)/events_per_block)
-    
-    from SequenceTaskParam import params,states,act
-    from SequenceTaskParam import Tloc,R,env_params
     #optionally add blocks of runs with D1 or D2 inactivated
     #update some parameters
     params['distance']='Euclidean'
     params['wt_learning']=False
-    params['events_per_trial']=events_per_trial
     params['decision_rule']=None #'delta'#'combo', 'delta', 'sumQ2', None means use choose_winner
-    params['Q2other']=0.1  #heterosynaptic syn plas of Q2 for other actions
+    params['Q2other']=0.0  #heterosynaptic syn plas of Q2 for other actions
     params['forgetting']=0#0.2 #heterosynaptic decrease Q1 for other actions
-    params['beta_min']=params['beta']
+    params['beta_min']=0.5#params['beta'] #0.1 is only slightly worse#
+    params['beta']=3
+    params['gamma']=0.95
+    params['beta_GPi']=10
+    params['moving_avg_window']=3 
     params['split']=True
-    params['decision_rule']=None#'delta'
+    params['rwd']=rwd['reward']
     #lower means more states
-    state_thresh={'Q1':[0.75,0],'Q2':[0.75,0.875]} #without normalized ED, with heterosynaptic LTP
-    if params['Q2other']<=0.05:
-        state_thresh={'Q1':[0.75,0],'Q2':[0.75,0.75]} #or st2=0.625 with normalized ED, with heterosynaptic LTP and LTD, q2o=0.05
-        alpha={'Q1':[0.2,0],'Q2':[0.2,0.25]} 
-    elif params['Q2other']==0.1:   
-        state_thresh={'Q1':[0.75,0],'Q2':[0.75,0.625]} #or st2= 0.875 with normalized ED, with heterosynaptic LTP and LTD, q2o=0.1
-        alpha={'Q1':[0.2,0],'Q2':[0.2,0.35]}    
-    elif params['Q2other']==0.2:
-        state_thresh={'Q1':[0.75,0],'Q2':[0.875,0.875]} #or st= 0.625,0.625 with normalized ED, with heterosynaptic LTP and LTD, q2o=0.2
-        alpha={'Q1':[0.2,0],'Q2':[0.2,0.3]}  #or alpha2=0.35 with st=0.625
+    state_thresh={'Q1':[0.75,0],'Q2':[0.75,0.875]} #without normalized ED, with heterosynaptic LTP 
+    state_thresh={'Q1':[0.75,0],'Q2':[0.75,0.625]} #or st2= 0.875 with normalized ED, with heterosynaptic LTD
+    alpha={'Q1':[0.2,0],'Q2':[0.2,0.35]}    
     #params['state_thresh']=[0.25,0.275]#[0.15,0.2] #threshold on prob for creating new state using Gaussian mixture
     # higher means more states. Adjusted so that in new context, Q2 creates new states, but not Q1
     #params['alpha']=[0.3,0.15] # [0.2,0.14] # double learning to learn in half the trials, slower for Q2 - D2 neurons
     output_data={q:{} for q in Qvalues}
-    all_Qhx={q:{} for q in Qvalues}
+    all_Qhx={q:[] for q in Qvalues}
     all_beta={q:[] for q in Qvalues}
     all_lenQ={q:{qq:[] for qq in range(1,q+1)} for q in Qvalues}
     numchars=6 
@@ -296,7 +296,6 @@ if __name__ == "__main__":
     resultslist={numQ:{k+'_'+ep:[] for k in sa_keys.values() for ep in epochs} for numQ in Qvalues}
     traject_dict={numQ:{k:[] for k in sa_keys.keys()} for numQ in Qvalues}
 
-
     for numQ in Qvalues:
         resultslist[numQ]['params']={p:[] for p in params}
         Qhx=None
@@ -311,15 +310,19 @@ if __name__ == "__main__":
             acq.episode(numevents,noise=noise,info=Info)
             if params['inact'] and numQ==2:
                 if params['inact']=='D2':
-                    acq.agent.numQ=1
+                    acq.agent.Q[1]=np.zeros(np.shape(acq.agent.Q[1]))
+                    acq.agent.alpha[1]=0
+                    #acq.agent.numQ=1
+                    params['Da_factor']=acq.agent.Da_factor=0.5
                 elif params['inact']=='D1':
                     acq.agent.Q[0]=np.zeros(np.shape(acq.agent.Q[0]))
                     acq.agent.alpha[0]=0
+                    params['Da_factor']=acq.agent.Da_factor=2                    
                 acq.episode(events_per_block*params['inact_blocks'],noise=noise,info=Info)
                 #acq.set_of_plots('LLRR, numQ='+str(params['numQ']),noise,Hx_len,hist=plot_hist)
             results[numQ]=acq.count_actions(results[numQ],state_action_combos,trial_subset,accum_type='mean')#,accum_type='count')
             traject_dict=acq.trajectory(traject_dict, sa_keys,num_blocks+params['inact_blocks'],events_per_block,numQ,accum_type='mean')#,accum_type='count')
-            if r<1:
+            if r<1 and other_plots:
                 acq.set_of_plots(str(numQ),noise,Hx_len,title2='',hist=plot_hist)
                 #acq.visual()
             if plot_Qhx: #need to return state_nums, which may differ for each run
@@ -330,7 +333,7 @@ if __name__ == "__main__":
             for qq in all_lenQ[numQ].keys():
                 if qq-1 in acq.agent.learn_hist['lenQ'].keys():
                     all_lenQ[numQ][qq].append(acq.agent.learn_hist['lenQ'][qq-1])
-        all_Qhx[numQ]=Qhx
+            all_Qhx[numQ]=Qhx
         resultslist=rlu.save_results(results,sa_keys,resultslist)      
         for p in resultslist[numQ]['params'].keys():               #
             resultslist[numQ]['params'][p].append(params[p])                #
@@ -339,39 +342,51 @@ if __name__ == "__main__":
         print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
         print(' Using',params['numQ'], 'Q, alpha=',params['alpha'],'thresh',params['state_thresh'], 'runs',runs,'of total events',numevents)
         print(' weights:',[k+':'+str(params[k]) for k in params.keys() if k.startswith('wt')])
-        print('Q2 hetero=',params['Q2other'],'decision rule=',params['decision_rule'])
+        print('Q2 hetero=',params['Q2other'],'decision rule=',params['decision_rule'],'beta=',params['beta_min'],params['beta'])
         print('counts from ',trial_subset,' events: BEGIN    END    std over ',runs,'runs. Hx_len=',Hx_len)
         norm={sac:100 for sac in results[numQ].keys()}
-        norm['rwd']=100/max_rwd
+        norm['rwd']=1
         for sa_combo,counts in results[numQ].items():
             print(sa_combo,':::',np.round(np.mean(counts['Beg'])*norm[sa_combo],2),'% +/-',np.round(np.std(counts['Beg'])*norm[sa_combo],2),
                     ',', np.round(np.mean(counts['End'])*norm[sa_combo],2),'% +/-',np.round(np.std(counts['End'])*norm[sa_combo],2))
-        for i in range(params['numQ']):
-            acq.agent.visual(acq.agent.Q[i],labels=acq.state_to_words(i,noise,numchars),title='numQ='+str(numQ)+',Q'+str(i),state_subset=state_subset)
+        if other_plots:
+            for i in range(params['numQ']):
+                acq.agent.visual(acq.agent.Q[i],labels=acq.state_to_words(i,noise,numchars),title='numQ='+str(numQ)+',Q'+str(i),state_subset=state_subset)
         
         if save_data:
             import datetime
             dt=datetime.datetime.today()
             date=str(dt).split()[0]
-            #fname='Sequence'+date+'DecisionRule'+str(params['decision_rule'])+'_numQ'+str(params['numQ'])+\
-            fname='Sequence'+date+'_HxLen'+str(Hx_len)+'_numQ'+str(params['numQ'])+'_alpha'+'_'.join([str(a) for a in params['alpha']])+\
-            '_st'+'_'.join([str(st) for st in params['state_thresh']])+'_q2o'+str(params['Q2other'])+'beta'+str(params['beta_min'])+'split'+str(params['split'])
+            key_params=['numQ','Q2other','beta_GPi','decision_rule','beta_min','beta','gamma','rwd']
+            fname_params=key_params+['split']
+            fname='Sequence'+date+'_'.join([k+str(params[k]) for k in fname_params])
+            #fname='Sequence'+date+'_HxLen'+str(Hx_len)+'_alpha'+'_'.join([str(a) for a in params['alpha']])+'_st'+'_'.join([str(st) for st in params['state_thresh']])+\
+
             if params['inact']:
-                fname=fname +'_inactive'+params['inact']
+                fname=fname +'_inactive'+params['inact']+'_'+str(params['Da_factor'])
             np.savez(fname,par=params,results=resultslist[numQ],traject=output_data[numQ],Qhx=all_Qhx[numQ],all_beta=all_beta[numQ],all_lenQ=all_lenQ[numQ],sa_errors=sa_errors)
             allQ={i:acq.agent.Q[i] for i in range(params['numQ'])}
             all_labels={i:acq.state_to_words(i,noise,numchars) for i in range(params['numQ'])}
             actions=acq.agent.actions
+    print('\nsummary for beta_min=',params['beta_min'],'beta_max=',params['beta'],'beta_GPi=', params['beta_GPi'],'gamma=',params['gamma'])
+    for numQ in Qvalues:
+        halfrwd=(np.max(output_data[numQ]['rwd']['mean'])+np.min(output_data[numQ]['rwd']['mean']))/2
+        ####### replace this with 90% of maximal to measure effect of beta? #############
+        block=np.min(np.where(output_data[numQ]['rwd']['mean']>halfrwd))
+        print('rwd End',':::',round(np.mean(results[numQ]['rwd']['End'])*norm['rwd'],2), \
+                'per trial, +/-',round(np.std(results[numQ]['rwd']['End'])*norm['rwd'],2), \
+                ', blocks to half reward=',block, 'for nQ=', numQ)
     #
-    title='History Length '+str(Hx_len)+'\nminimum '+str(events_per_trial)+' actions per reward'
-    rlu.plot_trajectory(output_data,title,[Qvalues])
+    title='History Length '+str(Hx_len)+'\nminimum '+str(params['events_per_trial'])+' actions per reward'
+    if other_plots:
+        rlu.plot_trajectory(output_data,title,[Qvalues])
     if plot_Qhx:
-        plot_states=[('Llever','RRLL'),('Rlever','LLLL'),('Rlever','RLLR'),('Rlever','RRLL')]
+        plot_states=[('Llever','--LL'),('Rlever','-LLR'),('Rlever','LLRR')]#[('Llever','RRLL'),('Rlever','LLLL'),('Rlever','RLLR'),('Rlever','RRLL')]
         actions_lines={a:'solid' for a in actions_colors.keys()}
         from TD2Q_Qhx_graphs import plot_Qhx_sequence, plot_Qhx_sequence_1fig
-        figs=plot_Qhx_sequence_1fig (all_Qhx,plot_states,actions_colors,events_per_trial,actions_lines)
+        figs=plot_Qhx_sequence_1fig (all_Qhx,plot_states,actions_colors,params['events_per_trial'],actions_lines)
         #for numQ,Qhx in all_Qhx.items():
-            #figs=plot_Qhx_sequence(Qhx,actions_colors,events_per_trial,numQ)
-            #figs=plot_Qhx_sequence_1fig (Qhx,plot_states,actions_colors,events_per_trial)
+            #figs=plot_Qhx_sequence(Qhx,actions_colors,params['events_per_trial'],numQ)
+            #figs=plot_Qhx_sequence_1fig (Qhx,plot_states,actions_colors,params['events_per_trial'])
 
 
